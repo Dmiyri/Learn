3a6424d4b987da06890fb12e5d9c193edc66dacd
Dmiyri <timmy.1989.89@gmail.com>
2020-02-26 21:36:20 +0300

    Updates

---
+++ precache-manifest.ce98df06b9632433917f9aa7bdf3ea95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24275e67861d858ba09904e618ebcf13",
+    "url": "/Learn/index.html"
+  },
+  {
+    "revision": "b44b9166b230c82fb6fd",
+    "url": "/Learn/static/css/main.581f4973.chunk.css"
+  },
+  {
+    "revision": "030a941794a2bcf1b645",
+    "url": "/Learn/static/js/2.280094fc.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Learn/static/js/2.280094fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b44b9166b230c82fb6fd",
+    "url": "/Learn/static/js/main.ffce7d65.chunk.js"
+  },
+  {
+    "revision": "f02cce46882e10390a60",
+    "url": "/Learn/static/js/runtime-main.f45cef5d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Learn/static/media/logo.5d5d9eef.svg"
+  }
+]);
